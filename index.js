9bd2e9df113eade664c73f37411abf1076e172e3
ankit-1308 <mauryaankit1308@gmail.com>
2024-04-10 19:58:50 +0530

    Mongodb cloud database setup successfully

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3001
 const cors = require("cors");
 const morgan = require("morgan");
 const express = require("express");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -20,6 +21,15 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    if (mongoose.connection.readyState === 1) {
+      next();
+    } else {
+      res.status(500);
+      res.json({ error: "Internal Server Error - Database Disconnected" });
+    }
+  });
+
 
 
 
@@ -29,12 +39,23 @@ app.use(function (err, req, res, next) {
   res.json({ error: err });
 });
 
+const { connectToDB } = require("./database");
 
 
 const startServer = async () => {
   app.listen(port, () => {
     console.log(`Server listinening on http://localhost:${port}`);
   });
+
+  try {
+    await connectToDB();
+    console.log("Database Connected.");
+  } catch (err) {
+    console.log(">ERROR :", err.name);
+    console.log(">Error Message :", err.message);
+    console.log(">Error Code :", err.code ? err.code : 0);
+    console.log(">Error CodeName :", err.codeName ? err.codeName : "null");
+  }
 
  
 };
