b2cc87774f73d716337b726db0500ab29b971ee5
pranjali1802 <76839816+pranjali1802@users.noreply.github.com>
2024-04-16 23:25:48 +0530

    Added api and its routing for updating an inventory

--- routers/inventory.js
+++ routers/inventory.js
@@ -3,5 +3,5 @@ const InventoryController = require("../controller/inventoryController.js");
 
 router.post("/create", InventoryController.createInventory);
 router.get("/get-inventory/:sku", InventoryController.getInventoryViaSku);
-
+router.put("/update-inventory/:sku", InventoryController.updateInventory)
 module.exports = router;
